648566aee035191daa94e1fe16b4cfc5fdde9b15
olha-polishchuk <109108651+olha-polishchuk@users.noreply.github.com>
2022-07-12 23:12:47 +0300

    JS bugs got fixed

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//   `It is currently ${Math.round(weather[city].temp)}°C
+// (${Math.round((weather[city].temp * 9) / 5 + 32)}°F)
+
 let currentTime = new Date();
 let change = document.querySelector("#change");
 let week = [
@@ -9,16 +12,44 @@ let week = [
   "Friday",
   "Saturday",
 ];
+
 let currentDay = week[currentTime.getDay()];
 let timeHour = currentTime.getHours();
+if (timeHour < 10) {
+  timeHour = `0${timeHour}`;
+}
 let timeMin = currentTime.getMinutes();
+if (timeMin < 10) {
+  timeMin = `0${timeMin}`;
+}
 change.innerHTML = `Today is ${currentDay}, ${timeHour}:${timeMin}`;
+
+function displayWeatherCondition(response) {
+  document.querySelector("#celsius").innerHTML = `☀ ${Math.round(
+    response.data.main.temp
+  )} °C`;
+  console.log(response);
+  document.querySelector(
+    "#humidity"
+  ).innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  document.querySelector("#wind").innerHTML = `Wind: ${Math.round(
+    response.data.wind.speed
+  )} km/h`;
+  document.querySelector(
+    "#clear"
+  ).innerHTML = `${response.data.weather[0].main} in <strong> ${response.data.name}</strong>`;
+}
+function searchCity(city) {
+  let apiKey = "1232f73dc68c5b5be0e1f667a96ca434";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
 
 function showCity(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input");
   let cityIndicators = document.querySelector("#clear");
-  cityIndicators.innerHTML = `Clear in ${cityInput.value}`;
+  cityIndicators.innerHTML = `Clear in <strong> ${cityInput.value} </strong>`;
 }
 let cityForm = document.querySelector("#city-form");
 cityForm.addEventListener("submit", showCity);
@@ -70,7 +101,7 @@ function showTempButton(response) {
   let city = response.data.name;
   let cityIndicators = document.querySelector("#clear");
   tempChange.innerHTML = `☀ ${temp}°C`;
-  cityIndicators.innerHTML = `Clear in ${city}`;
+  cityIndicators.innerHTML = `Clear in <strong> ${city} </strong>`;
   let cityBut = document.querySelector("button");
   cityBut.innerHTML = `${city}`;
 }
@@ -89,3 +120,5 @@ function getButton(event) {
 }
 let butChange = document.querySelector("button");
 butChange.addEventListener("click", getButton);
+
+searchCity("Kyiv");
